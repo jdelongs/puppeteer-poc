83a140db2c78854f9a6708528ad42e933b78a9f7
jdelongs <jdelong15@hotmail.ca>
2022-09-08 13:08:19 -0400

    removed depecated function

--- specs/users.test.js
+++ specs/users.test.js
@@ -1,20 +1,18 @@
-jest.setTimeout(60000)
+jest.setTimeout(60000);
 
 describe('Basic authentication e2e tests', () => {
-  beforeAll( async () => {
-  // Set a definite size for the page viewport so view is consistent across browsers
-    await page.setViewport( {
+  beforeAll(async () => {
+    // Set a definite size for the page viewport so view is consistent across browsers
+    await page.setViewport({
       width: 1366,
       height: 768,
-      deviceScaleFactor: 1
-    } );	
+      deviceScaleFactor: 1,
+    });
 
     await page.goto('https://www.google.com');
+  });
 
-    await page.waitFor(5000);
-    } );
-
-  it( 'Should be truthy', async () => {
-    expect( true ).toBeTruthy();
-  })	
+  it('Should be truthy', async () => {
+    expect(true).toBeTruthy();
+  });
 });
